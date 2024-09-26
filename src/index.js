a75e14af386193357697539e736d0fc941727fbd
Artem <artem.horevyi@snotor.pro>
2024-09-26 14:34:44 +0300

    setup git pages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/career-questionnaire-test-app/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
